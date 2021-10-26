d5e1c880802544dd4418907669ea2da06ed5809e
inyang <inyang@student.42seoul.kr>
2021-10-26 17:06:04 +0900

    dead time check

--- check_philo.c
+++ check_philo.c
@@ -6,7 +6,7 @@
 /*   By: inyang <inyang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 17:14:52 by inyang            #+#    #+#             */
-/*   Updated: 2021/10/26 02:59:12 by inyang           ###   ########.fr       */
+/*   Updated: 2021/10/26 17:04:34 by inyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 int	time_to_die_check(t_philo *philo, t_common *common)
 {
 	int	i;
-	int	time;
+	unsigned long	time;
 
 	i = -1;
 	time = 0;
 	while (++i < common->philo_numbers)
 	{
-		time = check_time() - philo[i].last_eat;
-		if (time >= common->time_to_die)
+		time = check_time();
+		if (time - philo[i].last_eat >= (unsigned long)(common->time_to_die))
 		{
 			common->check_flag = 1;
-			print_status(common, time, i + 1, DIE);
+			print_status(common, time - philo->common->base_time, i + 1, DIE);
 			return (1);
 		}
 		if (philo[i].eat_cnt == common->philo_must_eat)
